c1cabecf0485f94585929b2eca8644b49d23839d
NasyaZafira <nasya.zafira16@gmail.com>
2025-04-09 15:00:06 +0700

    add model window

---
+++ app/Http/Controllers/WindowOrderController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\WindowOrder;
+
+class WindowOrderController extends Controller
+{
+    /**
+     * Display all window orders
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $windowOrders = WindowOrder::latest()->paginate(10);
+
+        return view('window.index', compact('windowOrders'));
+    }
+}
