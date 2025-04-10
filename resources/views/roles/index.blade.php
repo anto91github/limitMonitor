6fd097eb502e6bbc4d784d246c0987fcb4d3d05d
BCASekuritas <it@bcasekuritas.co.id>
2025-04-10 15:58:39 +0700

    create role model, and show role in user

--- resources/views/roles/index.blade.php
+++ resources/views/roles/index.blade.php
@@ -22,32 +22,29 @@ Role list
             <table class="table table-striped">
                 <tr>
                     <th width="1%">No</th>
-                    <th>Name</th>
-                    <th>Permissions</th>
-                    <th width="3%" colspan="3">Action</th>
+                    <th >Name</th>
+                    {{-- <th>Permissions</th> --}}
+                    <th width="15%">Action</th>
                 </tr>
                 @foreach ($roles as $key => $role)
                 <tr>
                     <td>{{ $role->id }}</td>
                     <td>{{ $role->name }}</td>
-                    <td>
+                    {{-- <td>
                         @foreach ($role->permissions as $perm)
                         <span class="badge text-bg-info">{{ $perm->name }}</span>
                         @endforeach
-                    </td>
+                    </td> --}}
                     <td>
-                        <a class="btn btn-info btn-sm" href="{{ route('roles.show', $role->id) }}">Show</a>
-                    </td>
-                    <td>
+                        {{-- <a class="btn btn-info btn-sm" href="{{ route('roles.show', $role->id) }}">Show</a> --}}
                         <a class="btn btn-primary btn-sm" href="{{ route('roles.edit', $role->id) }}">Edit</a>
-                    </td>
-                    <td>
                         <form action="{{ route('roles.destroy', $role->id) }}" method="POST" style="display:inline;">
                             @csrf
                             @method('DELETE')
                             <button type="submit" class="btn btn-danger btn-sm">Delete</button>
                         </form>
                     </td>
+                   
                 </tr>
                 @endforeach
 
--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -38,15 +38,31 @@ Create User
                     @endif
                 </div>
                 <div class="mb-3">
-                    <label for="username" class="form-label">Username</label>
-                    <input value="{{ old('username') }}"
-                        type="text" 
+                    <label for="password" class="form-label">Password</label>
+                        <input 
+                            type="password" 
+                            class="form-control" 
+                            id="password"
+                            name="password" 
+                            placeholder="Password" required>                                       
+                </div>
+
+                <div class="mb-3">
+                    <label for="confirm_password" class="form-label">Confirm Password</label>
+                    <input 
+                        type="password" 
                         class="form-control" 
-                        name="username" 
-                        placeholder="Username" required>
-                    @if ($errors->has('username'))
-                        <span class="text-danger text-left">{{ $errors->first('username') }}</span>
-                    @endif
+                        name="confirm_password" 
+                        placeholder="Confirm Password" required>
+                </div>
+
+                <div class="mb-3">
+                    <label for="status" class="form-label">Is Active</label>
+                    <br/>
+                    <label class="switch">
+                        <input type="checkbox" class="toggle-active" name="is_active">
+                        <span class="slider round"></span>
+                    </label>
                 </div>
 
                 <button type="submit" class="btn btn-primary">Save user</button>
@@ -55,4 +71,51 @@ Create User
         </div>
 
     </div>
+
+    <style>
+        .switch {
+            position: relative;
+            display: inline-block;
+            width: 60px;
+            height: 34px;
+        }
+
+        .switch input {
+            opacity: 0;
+            width: 0;
+            height: 0;
+        }
+
+        .slider {
+            position: absolute;
+            cursor: pointer;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            background-color: #ccc;
+            transition: .4s;
+            border-radius: 34px;
+        }
+
+        .slider:before {
+            position: absolute;
+            content: "";
+            height: 26px;
+            width: 26px;
+            left: 4px;
+            bottom: 4px;
+            background-color: white;
+            transition: .4s;
+            border-radius: 50%;
+        }
+
+        input:checked + .slider {
+            background-color: #2196F3;
+        }
+
+        input:checked + .slider:before {
+            transform: translateX(26px);
+        }
+    </style>
 @endsection
