a58c02b72ce9d0b226b5fd3b1c45930959be9e27
BCASekuritas <it@bcasekuritas.co.id>
2025-04-14 14:33:45 +0700

    remove copy right in footer

--- resources/views/layouts/includes/footer.blade.php
+++ resources/views/layouts/includes/footer.blade.php
@@ -1,7 +1,10 @@
 <footer class="footer text-sm">
     <div>
-        <a href="/">Bilas</a>.
-        Copyright &copy; {{ date('Y') }}
+        {{-- <a href="/">Bilas</a>. --}}
+
+        {{-- Copyright &copy; {{ date('Y') }} --}}
     </div>
-    <div class="ms-auto">Bootstrap Admin Template</div>
+    <div class="ms-auto">
+        {{-- Bootstrap Admin Template --}}
+    </div>
 </footer>
