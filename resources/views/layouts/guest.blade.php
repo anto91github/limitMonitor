1b9c5e27194bd0784602f3ef266ab509f94e68ec
vinsen.wisnu <wisnuaji.johan@gmail.com>
2025-04-14 15:02:27 +0700

    add title login page

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>BCA Sekuritas</title>
+    <link rel="icon" href="{{ asset('icons/icon.png') }}">
     <meta name="theme-color" content="#ffffff">
     @vite('resources/sass/app.scss')
 </head>
